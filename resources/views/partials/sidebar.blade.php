d87f30f1dcb0befda56c3d27fa550732977189f7
Hamza-Mughales <hamzawemughales@gmail.com>
2021-07-15 12:29:44 +0300

    Users List and New Register Field (website)

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -52,20 +52,18 @@
                 </li>
             @endforeach
 
+            <li class="c-sidebar-nav-title">{{ __('Manage Data') }}</li>
+                <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
+                    <a class="c-sidebar-nav-link" href="{{ route('admin.users.index') }}">
+                        <svg class="c-sidebar-nav-icon">
+                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
+                        </svg> {{ __('Users') }}
+                    </a>
+                </li>
+
         @endif
 
-        <li class="c-sidebar-nav-title">{{ __('Other') }}</li>
-        <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link" href="{{ route('logout') }} "
-                onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
-                </svg> {{ __('Logout') }}
-            </a>
-        </li>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
+
     </ul>
 
 
